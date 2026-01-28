78ee49834e6898993733482f84a9d80dfb134c90
Yuanyang Cao <1130529700@qq.com>
2026-01-28 21:34:48 +0800

    aa
    Removed git clone command for e2e-metrics.

--- SAlora/bash/run_eval_e2e_large.sh
+++ SAlora/bash/run_eval_e2e_large.sh
@@ -18,7 +18,6 @@ echo "=========================================="
 
 if [ ! -d "e2e-metrics" ]; then
     echo ""
-    git clone https://github.com/tuetschek/e2e-metrics.git
     cd e2e-metrics
     pip install -r requirements.txt
     pip install future
