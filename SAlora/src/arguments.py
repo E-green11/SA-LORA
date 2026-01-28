7ef7f4e0f96272f11db7b58806fb79b95d380f73
Yuanyang Cao <1130529700@qq.com>
2026-01-28 21:49:59 +0800

    aa
    Removed docstring from DataTrainingArguments class.

--- SAlora/src/arguments.py
+++ SAlora/src/arguments.py
@@ -11,13 +11,6 @@ current_dir = os.path.dirname(__file__)
 
 @dataclass
 class DataTrainingArguments:
-    """
-    Arguments pertaining to what data we are going to input our model for training and eval.
-
-    Using `HfArgumentParser` we can turn this class
-    into argparse arguments to be able to specify them on
-    the command line.
-    """
 
     task_name: Optional[str] = field(
         default=None,
